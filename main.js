7b49f1ceadad9ca52a58a2ed11ff2a909e130103
Maxwell Russell <maxruss.87@gmail.com>
2016-10-17 15:13:32 -0500

    adjusting ajax rendering

--- main.js
+++ main.js
@@ -3,14 +3,26 @@ var settings = {
   type: 'GET',
   dataType: 'jsonp',
   success: function(data) {
-    console.log(data);
+    console.log(data.results);
+    var searchData = data.results;
+    searchData.map(createPage);
   },
   error: function(error) {
     console.log('error from api request' , error);
   }
 };
+$body = $('body');
+$searchResults = $('.searchResults');
 
-console.log($.ajax(settings));
-// API Key - 4umgg0ssq5zwuakpfxl6lfwd
-// API - no restriction: https://openapi.etsy.com/v2/listings/active?api_key=4umgg0ssq5zwuakpfxl6lfwd
-// API - restrictions added: https://openapi.etsy.com/v2/listings/active?api_key=4umgg0ssq5zwuakpfxl6lfwd&keywords=whiskey&includes=Images,Shop
+$.ajax(settings);
+
+function createPage(item, i, arr) {
+  // var $li = $('li');
+  var $createItem = $('<li></li>');
+  var $loadPicture = $('<img src="' + item.Images[0].url_fullxfull + '"/>');
+  var $loadTitle = $('<h2>' + item.title + '</h2>');
+  var $loadSeller = $('<span class="seller">' + item.Shop.shop_name + '</span>');
+  var $loadPrice = $('<span class="price">' + item.price + '</span>');
+  $body.append($createItem, $loadPicture, $loadTitle, $loadSeller, $loadPrice);
+
+}
