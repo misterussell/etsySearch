9be3ae923a4f14b260c539248263f9b5956e5d4a
Maxwell Russell <maxruss.87@gmail.com>
2016-10-18 07:01:59 -0500

    added search function

--- main.js
+++ main.js
@@ -1,28 +1,55 @@
-var settings = {
-  url: 'https://openapi.etsy.com/v2/listings/active.js?api_key=4umgg0ssq5zwuakpfxl6lfwd&keywords=whiskey&includes=Images,Shop',
-  type: 'GET',
-  dataType: 'jsonp',
-  success: function(data) {
-    console.log(data.results);
-    var searchData = data.results;
-    searchData.map(createPage);
-  },
-  error: function(error) {
-    console.log('error from api request' , error);
-  }
-};
-$body = $('body');
-$searchResults = $('.searchResults');
+function searchBar() {
+  var $searchBar = $('.searchBar');
 
-$.ajax(settings);
+  var createSearch = $('<form class="search" action="index.html" method="post"><input type="text" name="name" class="userSearch" value=""></form><button class="submit">Search</button>');
 
-function createPage(item, i, arr) {
-  // var $li = $('li');
-  var $createItem = $('<li></li>');
-  var $loadPicture = $('<img src="' + item.Images[0].url_fullxfull + '"/>');
-  var $loadTitle = $('<h2>' + item.title + '</h2>');
-  var $loadSeller = $('<span class="seller">' + item.Shop.shop_name + '</span>');
-  var $loadPrice = $('<span class="price">' + item.price + '</span>');
-  $body.append($createItem, $loadPicture, $loadTitle, $loadSeller, $loadPrice);
-
+  $searchBar.append(createSearch);
 }
+
+//LOAD INITIAL SEARCH
+searchBar();
+
+function settings(searchTerm) {
+  var settings = {
+    url: 'https://openapi.etsy.com/v2/listings/active.js?api_key=4umgg0ssq5zwuakpfxl6lfwd&keywords=' + searchTerm + '&includes=Images,Shop',
+    type: 'GET',
+    dataType: 'jsonp',
+    success: function(data) {
+      // console.log(data.results);
+      var searchData = data.results;
+      searchData.map(loadResults);
+    },
+    error: function(error) {
+      console.log('error from api request' , error);
+    }
+  };
+  return settings;
+}
+
+function loadResults(item, i, arr) {
+  // var $li = $('li');
+  var $h2 = $('h2');
+  // var $textLimit = $('.' + item.listing_id);
+  var $searchResults = $('.searchResults');
+
+  var $createItem = $('<a href="' + item.url + '"><li><img src="' + item.Images[0].url_170x135 + '"/><aside><h2 class="' + item.listing_id + '">' + item.title + '</h2><span class="seller">' + item.Shop.shop_name + '</span><span class="price">' + item.price + '</span></aside></li></a>');
+
+  $searchResults.append($createItem);
+
+  // $textLimit.text($textLimit.text().substring(0, 30));
+}
+// var searchBox = $('.userSearch');
+var submit = $('.submit');
+
+submit.on('click', function(e){
+  var newSearch = document.querySelector('.userSearch').value;
+  // console.log(newSearch);
+  searchTerm = newSearch;
+  if (!searchTerm) {
+    alert('The limit does not exist! Please search something valid.');
+  } else {
+  $.ajax(settings(searchTerm));
+  }
+});
+
+// $.ajax(settings(searchTerm));
